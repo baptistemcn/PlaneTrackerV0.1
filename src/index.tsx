91555efc4c1631db26ff2a71032261fba94dea85
Baptiste Marcon <baptistemcn@icloud.com>
2023-10-25 12:31:56 +0200

    feat(fonts): add sf fonts and loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { useFonts } from "expo-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { ReThemeProvider } from "./theme";
@@ -5,6 +6,16 @@ import { ReThemeProvider } from "./theme";
 import { PlaneTracker } from "./App";
 
 export const Entry = () => {
+  const [fontsLoaded] = useFonts({
+    Bold: require("../assets/fonts/SF-Pro-Text-Bold.otf"),
+    SemiBold: require("../assets/fonts/SF-Pro-Text-Semibold.otf"),
+    Regular: require("../assets/fonts/SF-Pro-Text-Regular.otf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
       <ReThemeProvider>
